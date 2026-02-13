b255b6773b907a9626e5215279b2529b5cdfac6d
Alexander Fokin <77535903+24tiy@users.noreply.github.com>
2026-02-13 12:29:34 +0100

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import Frame1 from "../imports/Frame155257698";
 
 export default function App() {
   return (
-    <div className="size-full">
+    <div className="size-full"> 
       <Frame1 />
     </div>
   );
